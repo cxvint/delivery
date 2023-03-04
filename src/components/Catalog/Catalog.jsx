e72d4be58abba6af32e3dc74cb90eff1582af83f
cxvint <cxvint@gmail.com>
2023-03-04 18:05:21 +0300

    add components

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,37 @@
+import { CatalogProduct } from '../CatalogProduct/CatalogProduct';
+import { Container } from '../Container/Container';
+import { Order } from '../Order/Order';
+import style from './Catalog.module.css';
+
+const goodsList = [
+	{ title: 'Мясная бомба' },
+	{ title: 'Супер сырный' },
+	{ title: 'Сытный' },
+	{ title: 'Итальянский' },
+	{ title: 'Вечная классика' },
+	{ title: 'Тяжелый удар' },
+];
+
+export const Catalog = () => {
+	return (
+		<section className={style.catalog}>
+			<Container className={style.container}>
+				<Order />
+
+				<div className={style.wrapper}>
+					<h2 className={style.title}>Бургеры</h2>
+
+					<div className={style.wrap_list}>
+						<ul className={style.list}>
+							{goodsList.map((item) => (
+								<li className={style.item}>
+									<CatalogProduct title={item.title} />
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
