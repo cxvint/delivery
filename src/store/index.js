1fda054d5f870830b499f705e6844fc2f5637281
cxvint <cxvint@gmail.com>
2023-06-10 18:53:45 +0300

    fix err index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoryReducer from './category/categorySlice';
 import productReducer, { productApi } from './product/productSlice';
-import orderReducer, { localStorageMiddleware } from './order/orderSlice';
+import orderReducer from './order/orderSlice';
 import favoriteReducer from './favorites/favoriteSlice';
 import modalReducer from './modalDelivery/modalDeliverySlice';
 import formReducer from './form/formSlice';
@@ -19,8 +19,5 @@ export const store = configureStore({
 		[productApi.reducerPath]: productApi.reducer,
 	},
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(
-			localStorageMiddleware,
-			productApi.middleware
-		),
+		getDefaultMiddleware().concat(productApi.middleware),
 });
